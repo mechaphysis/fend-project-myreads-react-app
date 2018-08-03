51b17e9b05cb31e38d91ed4119036341eb90bd1f
mechaphysis <triptofante@gmail.com>
2018-08-03 22:53:58 +0200

    refactor: Deestructure hardcoded App.js into React Components

---
+++ src/ListBooks.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import Book from './Book.js'
+
+class ListBooks extends Component {
+  render() {
+    return (
+      <div className="list-books">
+      <div className="list-books-title">
+      <h1>MyReads</h1>
+      </div>
+        <div className="list-books-content">
+          <div>
+
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Currently Reading</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    <li>
+                    <Book />
+                    </li>
+                  </ol>
+                </div>
+            </div>
+
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Want to Read</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    <li>
+                    <Book />
+                    </li>
+                  </ol>
+                </div>
+            </div>
+
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Read</h2>
+                <div className="bookshelf-books">
+                  <ol className="books-grid">
+                    <li>
+                    <Book />
+                    </li>
+                  </ol>
+                </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      // TODO: Substitute this open-search functionality for a Link component
+      // <div className="open-search">
+      // <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+      // </div>
+    );
+  }
+}
+
+export default ListBooks;
