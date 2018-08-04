c475f206a0e3979ab911e7552a7e02c4cdc56d7e
Esperanza Natalia González <38734470+mechaphysis@users.noreply.github.com>
2018-08-04 13:27:26 +0200

    fix: Correct props declaration

--- src/Book.js
+++ src/Book.js
@@ -9,11 +9,11 @@ class Book extends Component {
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnails})` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnails})` }}></div>
           <BookShelfChanger />
         </div>
-         <div className="book-title">{book.title}</div>
-         <div className="book-authors">{book.authors}</div>
+         <div className="book-title">{this.props.book.title}</div>
+         <div className="book-authors">{this.props.book.authors}</div>
       </div>
 
     );
