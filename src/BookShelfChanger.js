70332cc9700e2addd5beeabe912671376c0c0947
mechaphysis <triptofante@gmail.com>
2018-08-06 13:00:49 +0200

    fix: Correct shelfChanger typo on calling method at BookShelfChanger

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -5,7 +5,7 @@ class BookShelfChanger extends Component {
   render() {
     return (
       <div className="book-shelf-changer">
-        <select onChange={this.props.shelfChanger}>
+        <select onChange={this.props.shelfChanger()}>
           <option value="move" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
